651cf1f4f61ad5490d48d3a4b42ba89be5239050
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2026-06-26 17:03:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,6 @@
 # CONFIG
 # ==========================================================
 
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwbdG35W2hI6zc7hC5wIdGPbBY0nl_wbK9J1NB1Rfv1nYR3bbc4E51Wj4fmfjWBpdkq/exec"
+import streamlit as st
+
+WEB_APP_URL = st.secrets["https://script.google.com/macros/s/AKfycbwbdG35W2hI6zc7hC5wIdGPbBY0nl_wbK9J1NB1Rfv1nYR3bbc4E51Wj4fmfjWBpdkq/exec"]
