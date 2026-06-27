9a75076b2f92599c3bc15da657bf6eb6007b276e
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2026-06-27 08:13:48 +0800

    Update soalan.py

--- soalan.py
+++ soalan.py
@@ -6,7 +6,7 @@ SOALAN = {
         ("PDPA2","2. Bahasa yang digunakan adalah jelas. | The language used is clear."),
         ("PDPA3","3. Susunan item adalah sistematik. | The arrangement of items is systematic."),
         ("PDPA4","4. Bilangan item adalah bersesuaian. | The number of items is appropriate."),
-        ("PDPA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Assessment.")
+        ("PDPA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Evaluation.")
     ],
 
     "lm":[
@@ -15,7 +15,7 @@ SOALAN = {
         ("LMA2","2. Bahasa yang digunakan adalah jelas. | The language used is clear."),
         ("LMA3","3. Susunan item adalah sistematik. | The arrangement of items is systematic."),
         ("LMA4","4. Bilangan item adalah bersesuaian. | The number of items is appropriate."),
-        ("LMA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Assessment.")
+        ("LMA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Evaluation.")
 
     ],
 
@@ -25,7 +25,7 @@ SOALAN = {
         ("LIA2","2. Bahasa yang digunakan adalah jelas. | The language used is clear."),
         ("LIA3","3. Susunan item adalah sistematik. | The arrangement of items is systematic."),
         ("LIA4","4. Bilangan item adalah bersesuaian. | The number of items is appropriate."),
-        ("LIA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Assessment.")
+        ("LIA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Evaluation.")
 
     ],
 
@@ -35,7 +35,7 @@ SOALAN = {
         ("FYPA2","2. Bahasa yang digunakan adalah jelas. | The language used is clear."),
         ("FYPA3","3. Susunan item adalah sistematik. | The arrangement of items is systematic."),
         ("FYPA4","4. Bilangan item adalah bersesuaian. | The number of items is appropriate."),
-        ("FYPA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Assessment.")
+        ("FYPA5","5. Instrumen ini memenuhi tujuan Penilaian Pembelajaran. | This instrument meets the purpose of Learning Evaluation.")
 
     ]
 
